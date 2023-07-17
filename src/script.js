8e2769fbc526ee67926cf196e0b49775329f19cf
Vkscoma <vkscoma@gmail.com>
2023-07-17 16:36:19 -0700

    Updating base files and added starter JS

--- src/script.js
+++ src/script.js
@@ -1,1 +1,16 @@
-const test = document.querySelector('.test');
+// Gloabal Variables
+const searchBtn = document.getElementById("search-btn");
+
+//Dictionary API Fetch
+function getDefinition(word) {
+    fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+        .then((response) => response.json())
+        .then((data) => console.log(data));
+}
+
+searchBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    const word = document.getElementById("default-search");
+    getDefinition(word.value);
+    word.value = "";
+});
