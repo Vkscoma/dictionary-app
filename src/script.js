9359aa556c3dfbfa3e0778c28f6bf944ecd57035
Vkscoma <vkscoma@gmail.com>
2023-07-18 09:15:09 -0700

    updating scripts

--- src/script.js
+++ src/script.js
@@ -5,7 +5,29 @@ const searchBtn = document.getElementById("search-btn");
 function getDefinition(word) {
     fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
         .then((response) => response.json())
-        .then((data) => console.log(data));
+        .then((data) => {
+            // Definition console.log(data[0].meanings[0].definitions[0].definition);
+            // console.log(data);
+            // Update DOM with the Word
+            const keyWord = document.getElementById("main--word");
+            keyWord.innerText = data[0].word.slice(0, 1).toUpperCase() + data[0].word.slice(1);
+            // Update DOM with the Definition
+            const definitionElement = document.querySelector("#definition--list");
+            console.log(data[0].meanings[0].definitions);
+            const definitionArray = data[0].meanings[0].definitions;
+            if (definitionArray.length > 1) {
+                for (let i = 0; i < definitionArray.length; i++) {
+                    const definitionLI = document.createElement("li");
+                    definitionLI.innerText = definitionArray[i].definition;
+                    definitionElement.appendChild(definitionLI);
+                }
+            } else {
+                const definitionLI = document.createElement("li");
+                definitionLI.innerText = definitionArray[0].definition;
+                definitionElement.appendChild(definitionLI);
+            }
+        });
+
 }
 
 searchBtn.addEventListener("click", (event) => {
