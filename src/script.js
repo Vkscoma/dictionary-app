297e1dfd10939232a4c2415c82e7e3f9e22fd576
Vkscoma <vkscoma@gmail.com>
2023-07-18 10:47:20 -0700

    updating script/styling

--- src/script.js
+++ src/script.js
@@ -1,30 +1,40 @@
 // Gloabal Variables
 const searchBtn = document.getElementById("search-btn");
 
-//Dictionary API Fetch
+//Dictionary API
 function getDefinition(word) {
     fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
         .then((response) => response.json())
         .then((data) => {
-            // Definition console.log(data[0].meanings[0].definitions[0].definition);
-            // console.log(data);
+            console.log(data)
             // Update DOM with the Word
             const keyWord = document.getElementById("main--word");
             keyWord.innerText = data[0].word.slice(0, 1).toUpperCase() + data[0].word.slice(1);
             // Update DOM with the Definition
             const definitionElement = document.querySelector("#definition--list");
-            console.log(data[0].meanings[0].definitions);
+            // console.log(data[0].meanings[0].definitions);
             const definitionArray = data[0].meanings[0].definitions;
             if (definitionArray.length > 1) {
                 for (let i = 0; i < definitionArray.length; i++) {
                     const definitionLI = document.createElement("li");
+                    definitionLI.classList.add("my-2");
                     definitionLI.innerText = definitionArray[i].definition;
                     definitionElement.appendChild(definitionLI);
                 }
             } else {
                 const definitionLI = document.createElement("li");
-                definitionLI.innerText = definitionArray[0].definition;
+                definitionLI.innerText = "No definition found";
                 definitionElement.appendChild(definitionLI);
+            }
+            // Render Synonyms of the word
+            const synonymElement = document.querySelector("#synonyms--element");
+            // Render Example of the word
+            const exampleElement = document.querySelector("#example--list ");
+            const exampleSample = data[0].meanings[1].definitions[0].example;
+            if (exampleSample) {
+                const exampleLI = document.createElement("li");
+                exampleLI.innerText = exampleSample;
+                exampleElement.appendChild(exampleLI);
             }
         });
 
@@ -33,6 +43,8 @@ function getDefinition(word) {
 searchBtn.addEventListener("click", (event) => {
     event.preventDefault();
     const word = document.getElementById("default-search");
+    const definitionElement = document.querySelector("#definition--list");
     getDefinition(word.value);
     word.value = "";
+    definitionElement.innerText = "";
 });
